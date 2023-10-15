56affad60385601fccfa20d33f415794bf71ec3a
doc-gif_IshitobiHyo <tobiyamukunn118@gmail.com>
2023-10-15 18:09:28 +0900

    Initial commit

---
+++ js/judge_user_agent.js
@@ -0,0 +1,18 @@
+window.My_APP = window.My_APP || {};
+
+My_APP.UserAgent = (() => {
+    let ua = navigator.userAgent;
+    if (ua.indexOf('Android') > 0 && ua.indexOf('Mobile') === -1) {
+        return 0;
+    } else if ((ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0)) {
+        return 0;
+    } else if (ua.indexOf('iPhone') > 0) {
+        return 1;
+    } else if (ua.indexOf('iPad') > 0) {
+        return 1;
+    } else if (ua.indexOf('iPod') > 0) {
+        return 1;
+    } else {
+        return -1;
+    }
+});
